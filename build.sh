5c61aa73ac1a9b82c5e93a60a4023f8e2ce1b7ff
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-05-27 02:13:55 +0200

    Better but yet not working build script, that launches compiler and tries to copy generated file to host machine

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 # https://github.com/jtransc/gdx-backend-jtransc/issues/4
-docker run -t -i mhaye/lljvm /bin/bash
-
+#docker run -t -i mhaye/lljvm /bin/bash
+#docker run -i -v `pwd`/freetype2:/project -t mhaye/lljvm /bin/bash -c lljvm-cc -Iinclude src/base/ftapi.c
+#export PATH=/usr/local/lib/lljvm:$PATH
+rm lljvm.cid
+docker run \
+	--cidfile "lljvm.cid" \
+	-i -v `pwd`/freetype2:/project \
+	-t mhaye/lljvm \
+	/usr/local/lib/lljvm/lljvm-cc \
+		-I/project/include \
+		/project/src/base/ftapi.c \
+		-o /project/freetype2.class
+CID=`cat lljvm.cid`
+rm lljvm.cid
+docker cp $CID:/project/freetype2.class freetype2.class
